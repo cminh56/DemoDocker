8901972edf9ecc8d968c5b14b64e74f95c3c2f8c
chinhminh <chinhminh2003@gmail.com>
2025-06-13 08:29:38 +0700

    docker api

--- DemoDocker/DemoDocker/Program.cs
+++ DemoDocker/DemoDocker/Program.cs
@@ -1,17 +1,35 @@
-using Microsoft.EntityFrameworkCore;
-using DemoDocker_Infrastructure.Data;
-using DemoDocker_Domain.IRepositories;
-using DemoDocker_Infrastructure.Repositories;
 using DemoDocker_Application.Services;
+using DemoDocker_Domain.IRepositories;
+using DemoDocker_Infrastructure.Data;
+using DemoDocker_Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "DemoDocker API",
+        Version = "v1"
+    });
+});
 
 // Add DbContext
 builder.Services.AddDbContext<DataContext>(options =>
@@ -25,17 +43,31 @@ builder.Services.AddScoped<ProductService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Always use Swagger in Docker (remove environment check)
+app.UseSwagger(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
+    {
+        swaggerDoc.Servers = new List<Microsoft.OpenApi.Models.OpenApiServer>
+        {
+            new Microsoft.OpenApi.Models.OpenApiServer { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}" }
+        };
+    });
+});
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+// Use CORS before other middleware
+app.UseCors();
 
+// app.UseHttpsRedirection(); 
 app.UseAuthorization();
-
 app.MapControllers();
+
+// Apply migrations automatically (for development)
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DataContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
